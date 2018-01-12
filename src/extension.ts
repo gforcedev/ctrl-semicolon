7ee1c21f236d25f37c688292fa96de1cbf18c4db
Gforcedev <tecshotclark@gmail.com>
2018-01-12 20:51:40 +0000

    Working if no text selected

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,58 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "ctrl-semicolon" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let insertSemiColonCommand = vscode.commands.registerCommand('extension.insertSemiColon', () => {
         // The code you place here will be executed every time your command is executed
+        // current editor
+        const editor = vscode.window.activeTextEditor;
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        // check if there is no selection
+        if (editor.selection.isEmpty) {
+            // the Position object gives you the line and character where the cursor is
+            const position = editor.selection.active;
+
+            let line = editor.document.lineAt(position.line);
+            let text = line.text;
+
+            if (text.charAt(text.length - 1) != ';') {
+                const edit = new vscode.WorkspaceEdit();
+                edit.insert(editor.document.uri, line.range.end, ';');
+
+                return vscode.workspace.applyEdit(edit)
+            }
+        }
     });
 
-    context.subscriptions.push(disposable);
+    let removeSemiColonCommand = vscode.commands.registerCommand('extension.removeSemiColon', () => {
+        // The code you place here will be executed every time your command is executed
+        // current editor
+        const editor = vscode.window.activeTextEditor;
+
+        // check if there is no selection
+        if (editor.selection.isEmpty) {
+            // the Position object gives you the line and character where the cursor is
+            const position = editor.selection.active;
+
+            let line = editor.document.lineAt(position.line);
+            let text = line.text;
+
+            if (text.charAt(text.length - 1) == ';') {
+                let range = new vscode.Range(line.lineNumber, text.length - 1,
+                     line.lineNumber, text.length);
+                const edit = new vscode.WorkspaceEdit();
+                edit.delete(editor.document.uri, range);
+
+                return vscode.workspace.applyEdit(edit);
+            }
+        }
+
+    });
+
+    context.subscriptions.push(insertSemiColonCommand);
+    context.subscriptions.push(removeSemiColonCommand);
 }
 
 // this method is called when your extension is deactivated
